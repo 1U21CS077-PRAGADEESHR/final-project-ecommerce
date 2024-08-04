9f68825fc5edc92dc8d86a64af531140f519d39f
Pragadeesh <pragadeeshraguraman@gmail.com>
2024-08-04 22:27:30 +0530

    first commit

---
+++ src/app/admin-view/add-product/page.js
@@ -0,0 +1,229 @@
+"use client";
+
+import InputComponent from "@/components/FormElements/InputComponent";
+import SelectComponent from "@/components/FormElements/SelectComponent";
+import TileComponent from "@/components/FormElements/TileComponent";
+import ComponentLevelLoader from "@/components/Loader/componentlevel";
+import Notification from "@/components/Notification";
+import axios from "axios";
+import { GlobalContext } from "@/context";
+import { addNewProduct, updateAProduct } from "@/services/product";
+import {
+  AvailableSizes,
+  adminAddProductformControls,
+  firebaseConfig,
+  firebaseStroageURL,
+} from "@/utils";
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { resolve } from "styled-jsx/css";
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, firebaseStroageURL);
+
+const createUniqueFileName = (getFile) => {
+  const timeStamp = Date.now();
+  const randomStringValue = Math.random().toString(36).substring(2, 12);
+  return `${getFile.name}-${timeStamp}-${randomStringValue}`;
+};
+
+async function helperForUploadingImageToFirebase(file) {
+  const getFileName = createUniqueFileName(file);
+  const storageReference = ref(storage, `ecommerce/${getFileName}`);
+  const uploadImage = uploadBytesResumable(storageReference, file);
+
+  return new Promise((resolve, reject) => {
+    uploadImage.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadImage.snapshot.ref)
+          .then((downloadUrl) => resolve(downloadUrl))
+          .catch((error) => reject(error));
+      }
+    );
+  });
+}
+
+export default function AdminAddNewProduct() {
+  const initialFormData = {
+    name: "",
+    price: 0,
+    description: "",
+    category: "men",
+    sizes: [],
+    deliveryInfo: "",
+    onSale: "no",
+    imageUrl: "",
+    priceDrop: 0,
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [image, setImage] = useState(null);
+  const { componentLevelLoader, setComponentLevelLoader, currentUpdatedProduct, setCurrentUpdatedProduct } = useContext(GlobalContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (currentUpdatedProduct !== null) setFormData(currentUpdatedProduct);
+  }, [currentUpdatedProduct]);
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleButton = async () => {
+    if (!image) {
+      toast.error("Please select an image to upload.", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", "my_uploads");
+
+    try {
+      const response = await axios.post("https://api.cloudinary.com/v1_1/dqm2u33qv/image/upload", formData);
+      console.log(response);
+      setFormData((prev) => ({ ...prev, imageUrl: response.data.secure_url }));
+      toast.success("Image uploaded successfully.", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } catch (e) {
+      console.log("error", e);
+      toast.error("Error uploading image.", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  function handleTileClick(getCurrentItem) {
+    let cpySizes = [...formData.sizes];
+    const index = cpySizes.findIndex((item) => item.id === getCurrentItem.id);
+
+    if (index === -1) {
+      cpySizes.push(getCurrentItem);
+    } else {
+      cpySizes = cpySizes.filter((item) => item.id !== getCurrentItem.id);
+    }
+
+    setFormData({
+      ...formData,
+      sizes: cpySizes,
+    });
+  }
+
+  async function handleAddProduct() {
+    setComponentLevelLoader({ loading: true, id: "" });
+    const res =
+      currentUpdatedProduct !== null
+        ? await updateAProduct(formData)
+        : await addNewProduct(formData);
+
+    console.log(res);
+
+    if (res.success) {
+      setComponentLevelLoader({ loading: false, id: "" });
+      toast.success(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+
+      setFormData(initialFormData);
+      setCurrentUpdatedProduct(null);
+      setTimeout(() => {
+        router.push("/admin-view/all-products");
+      }, 1000);
+    } else {
+      toast.error(res.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setComponentLevelLoader({ loading: false, id: "" });
+      setFormData(initialFormData);
+    }
+  }
+
+
+  return (
+    <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
+      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
+          <input
+            accept="image/*"
+            max="1000000"
+            type="file"
+            onChange={handleImageChange}
+          />
+          <button onClick={handleButton}>Upload</button>
+
+          <div className="flex gap-2 flex-col">
+            <label>Available sizes</label>
+            <TileComponent
+              selected={formData.sizes}
+              onClick={handleTileClick}
+              data={AvailableSizes}
+            />
+          </div>
+          {adminAddProductformControls.map((controlItem) =>
+            controlItem.componentType === "input" ? (
+              <InputComponent
+                type={controlItem.type}
+                placeholder={controlItem.placeholder}
+                label={controlItem.label}
+                value={formData[controlItem.id]}
+                onChange={(event) => {
+                  setFormData({
+                    ...formData,
+                    [controlItem.id]: event.target.value,
+                  });
+                }}
+              />
+            ) : controlItem.componentType === "select" ? (
+              <SelectComponent
+                label={controlItem.label}
+                options={controlItem.options}
+                value={formData[controlItem.id]}
+                onChange={(event) => {
+                  setFormData({
+                    ...formData,
+                    [controlItem.id]: event.target.value,
+                  });
+                }}
+              />
+            ) : null
+          )}
+          <button
+            onClick={handleAddProduct}
+            className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide"
+          >
+            {componentLevelLoader && componentLevelLoader.loading ? (
+              <ComponentLevelLoader
+                text={currentUpdatedProduct !== null ? 'Updating Product' : "Adding Product"}
+                color={"#ffffff"}
+                loading={componentLevelLoader && componentLevelLoader.loading}
+              />
+            ) : currentUpdatedProduct !== null ? (
+              "Update Product"
+            ) : (
+              "Add Product"
+            )}
+          </button>
+        </div>
+      </div>
+      <Notification />
+    </div>
+  );
+}
